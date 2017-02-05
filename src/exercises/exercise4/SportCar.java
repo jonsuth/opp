e393a133c7dbf587c477919f62ae41adae2b35ea
jonsuth <jonathan.suthakaran@gmail.com>
2017-02-05 17:22:02 +0000

    Done ex4

---
+++ src/exercises/exercise4/SportCar.java
@@ -0,0 +1,22 @@
+package exercises.exercise4;
+
+/**
+ * Created by Jon Suthakaran on 05/02/2017.
+ */
+public class SportCar extends Vehicle {
+
+    private Double topSpeed;
+
+    public SportCar(Double horsepower, Double weight, Double topSpeed) {
+        super(horsepower, 0.5, weight);
+        this.topSpeed = topSpeed;
+    }
+
+    public Double getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(Double topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+}
---
+++ src/exercises/exercise4/TestConsumption.java
@@ -0,0 +1,28 @@
+package exercises.exercise4;
+
+import java.util.DoubleSummaryStatistics;
+
+/**
+ * Created by Jon Suthakaran on 05/02/2017.
+ */
+public class TestConsumption {
+
+    public static void main(String[] args) {
+        SportCar car1 = new SportCar(200.0, 1500.0, 220.0);
+        SportCar car2 = new SportCar(100.0, 1000.0, 170.0);
+        SportCar car3 = new SportCar(135.0, 1100.2, 173.0);
+
+        computeFuelCompumtion(car1);
+        computeFuelCompumtion(car2);
+        computeFuelCompumtion(car3);
+
+    }
+
+    public static void computeFuelCompumtion(SportCar car) {
+        System.out.println(
+                (1000 + (car.getWeight()/5))*
+                (car.getTopSpeed()/100)*
+                (car.getAerodynamics()*car.getHorsepower())/1000
+        );
+    }
+}
